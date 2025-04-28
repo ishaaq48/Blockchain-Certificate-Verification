ea5480f5708bb123d7b007bd7c0e23577f736144
Syed Ishaaq <syedishaaq48@gmail.com>
2025-04-28 15:42:59 +0530

    frontend integration

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@
 import express from "express"
 import dotenv from "dotenv"
 import cookieParser from "cookie-parser"
+import cors from 'cors'
 
 // file imports
 import connectDB from "./config/db.js"
@@ -13,7 +14,15 @@ dotenv.config()
 
 const app = express()
 
+
 // middleware
+
+app.use(cors({
+  origin: "http://localhost:3000",
+  credentials: true
+}));
+
+
 app.use(express.json());
 app.use(cookieParser());
 
